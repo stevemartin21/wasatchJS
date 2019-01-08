a5446344ca477a70eebcb3a08682fa1890880852
Steve Martin <stevemartintech2018@gmail.com>
2019-01-07 18:52:43 -0700

    Last commmit
    project is set up and ready to rock and roll tomorrow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 
 import { MDBSpinningPreloader, MDBBootstrapModulesPro, ToastModule } from 'ng-uikit-pro-standard';
+import { NavigationComponent } from './webSiteComponents/navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './webSiteComponents/home/home.component';
+import { JumbotronComponent } from './webSiteComponents/jumbotron/jumbotron.component';
+import { CreateComponent } from './services/create/create.component';
+import { ContactComponent } from './webSiteComponents/contact/contact.component';
+import { BlogComponent } from './webSiteComponents/blog/blog.component';
+import { DashboardComponent } from './appComponents/dashboard/dashboard.component';
+import { LoginComponent } from './authComponents/login/login.component';
+import { RegisterComponent } from './authComponents/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    JumbotronComponent,
+    CreateComponent,
+    ContactComponent,
+    BlogComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpModule,
     ToastModule.forRoot(),
@@ -23,7 +43,8 @@ import { MDBSpinningPreloader, MDBBootstrapModulesPro, ToastModule } from 'ng-ui
     AgmCoreModule.forRoot({
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en#key
       apiKey: 'Your_api_key'
-    })
+    }),
+    AppRoutingModule
   ],
   providers: [MDBSpinningPreloader],
   bootstrap: [AppComponent],
